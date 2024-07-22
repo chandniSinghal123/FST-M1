a55600a82bdb0144cc03cb9201327e14c6163617
chandniSinghal123 <chandnsi@in.ibm.com>
2024-07-22 18:38:49 +0530

    Add files via upload

---
+++ Python/Activities/activity24_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+# Set up the paremeterized test method
+@pytest.mark.parametrize("earned, spent, expected", [ (30, 10, 20), (20, 2, 18), ])
+def test_transactions(wallet_amount, earned, spent, expected):
+
+    # Add money to your wallet
+    wallet_amount += earned
+    print(wallet_amount)
+
+    # Subtract amount from wallet
+    wallet_amount -= spent
+    print(wallet_amount)
+
+    # Assertion
+    assert wallet_amount == expected
